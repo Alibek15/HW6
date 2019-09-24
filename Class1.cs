d38298fa193a8db5be2da6533ad080e1462da8b4
Alibek <52044412+Alibek15@users.noreply.github.com>
2019-09-24 21:38:17 +0600

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp21
+{
+    class Class1
+    {
+        private int[] _array;
+
+        public Class1()
+        {
+            Size = 1;
+            _array = new int[Size];
+        }
+
+        public Class1(int size)
+        {
+            Size = size;
+            _array = new int[Size];
+        }
+
+        public int Size { get; set; }
+
+        public int this[int i]
+        {
+            get
+            {
+                return _array[i];
+            }
+            set
+            {
+                _array[i] = (int)Math.Pow((double)value, 2);
+            }
+        }
+    }
+}
